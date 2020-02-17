4b7639d6e628049c768f102d0102bb2c2e8ef9e5
Thomas Luk <wing.chungluk@faber.work>
2020-02-17 17:20:34 +0000

    add comment

--- src/main/java/com/thomaslukwork/noobchain/NoobChain.java
+++ src/main/java/com/thomaslukwork/noobchain/NoobChain.java
@@ -19,18 +19,19 @@ public class NoobChain {
   public static Transaction genesisTransaction;
 
   public static void main(String[] args) {
+    //Setup Bouncey castle as a Security Provider
     Security.addProvider(new BouncyCastleProvider());
-
+    //Create the new wallets
     walletA = new Wallet();
     walletB = new Wallet();
-
+    //Test public and private keys
     System.out.println("Private and public keys:");
     System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
     System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
-
+    //Create a test transaction from WalletA to walletB
     Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
     transaction.generateSignature(walletA.privateKey);
-
+    //Verify the signature works and verify it from the public key
     System.out.println("Is signature verified");
     System.out.println(transaction.verifySignature());
 
@@ -67,7 +68,7 @@ public class NoobChain {
     System.out.println(blockChainJson);
   }
 */
-
+/*
   public static boolean isChainValid() {
     Block currentBlock;
     Block previousBlock;
@@ -93,6 +94,6 @@ public class NoobChain {
       }
     }
     return true;
-  }
+  }*/
 
 }
